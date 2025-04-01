157e8d5927e48e74b7e865e57686ad12490955bc
sibanarayan10 <sibanarayan014@gmail.com>
2025-04-01 22:49:55 +0530

    update:design and logo

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,45 +1,47 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
     <div>
-      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
-
-        <div>
-            <img src={assets.logo} className='mb-5 w-32' alt="" />
-            <p className='w-full md:w-2/3 text-gray-600'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            </p>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_2fr] gap-14 my-10 mt-40 text-sm  ">
+        <div className="flex items-center justify-center gap-4">
+          <img src={assets.logo} className="mb-5 w-32" alt="" />
+          <p className="w-full md:w-2/3 text-gray-600 ">
+            Our journey began with a simple idea: to provide a platform where
+            customers can easily discover, explore, and purchase a wide range of
+            products from the comfort of their homes.
+          </p>
         </div>
-
-        <div>
-            <p className='text-xl font-medium mb-5'>COMPANY</p>
-            <ul className='flex flex-col gap-1 text-gray-600'>
-                <li>Home</li>
-                <li>About us</li>
-                <li>Delivery</li>
-                <li>Privacy policy</li>
+        <div className="flex-row flex justify-center max-[635px]:gap-20  gap-10">
+          <div>
+            <p className="text-xl font-medium mb-5">COMPANY</p>
+            <ul className="flex flex-col gap-1 text-gray-600">
+              <li>Home</li>
+              <li>About us</li>
+              <li>Delivery</li>
+              <li>Privacy policy</li>
             </ul>
-        </div>
+          </div>
 
-        <div>
-            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-            <ul className='flex flex-col gap-1 text-gray-600'>
-                <li>+1-212-456-7890</li>
-                <li>contact@foreveryou.com</li>
+          <div>
+            <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+            <ul className="flex flex-col gap-1 text-gray-600">
+              <li>+1-212-456-7890</li>
+              <li>contact@stylo.com</li>
             </ul>
+          </div>
         </div>
-
       </div>
 
-        <div>
-            <hr />
-            <p className='py-5 text-sm text-center'>Copyright 2024@ forever.com - All Right Reserved.</p>
-        </div>
-
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2024@stylo.com - All Right Reserved.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
